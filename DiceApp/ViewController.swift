3ea3e7c19e29110e9b0f593c68151563ca43972f
Karol Bryzgiel <karol.bryzgiel@gmail.com>
2020-02-01 10:10:37 +0100

    Initial Commit

---
+++ DiceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceApp
+//
+//  Created by Karol Bryzgiel on 01/02/2020.
+//  Copyright © 2020 Karol Bryzgiel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
